c079b5843ae79aaa54669c971bc6088b0094ac7c
Sterling Greene <sterling@gradle.com>
2020-08-18 23:30:22 -0400

    Add integ test demonstrating shading problems with included build too

--- subprojects/composite-builds/src/integTest/groovy/org/gradle/integtests/composite/CompositeBuildPluginDevelopmentIntegrationTest.groovy
+++ subprojects/composite-builds/src/integTest/groovy/org/gradle/integtests/composite/CompositeBuildPluginDevelopmentIntegrationTest.groovy
@@ -16,6 +16,7 @@
 
 package org.gradle.integtests.composite
 
+import groovy.transform.NotYetImplemented
 import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import org.gradle.integtests.fixtures.build.BuildTestFile
 import spock.lang.Issue
@@ -59,6 +60,35 @@ class CompositeBuildPluginDevelopmentIntegrationTest extends AbstractCompositeBu
 
         then:
         executed ":pluginBuild:jar", ":pluginBuild:assemble", ":assemble"
+
+        where:
+        pluginsBlock | withVersion
+        true         | true
+        true         | false
+        false        | true
+        false        | false
+    }
+
+
+    @Unroll
+    @ToBeFixedForInstantExecution
+    @NotYetImplemented
+    def "does not expose Gradle runtime dependencies without shading"() {
+        given:
+        applyPlugin(buildA, pluginsBlock, withVersion)
+        addLifecycleTasks(buildA)
+
+        includeBuild pluginBuild
+
+        buildA.buildFile << """
+            import ${com.google.common.collect.ImmutableList.name}
+        """
+
+        when:
+        fails(buildA, "taskFromPluginBuild")
+
+        then:
+        failure.assertHasDescription("Could not compile build file '$buildA.buildFile.canonicalPath'.")
 
         where:
         pluginsBlock | withVersion
